374ec244e7da038ce139d0a915f00e9319ecb5cb
kondasg <gabor@kondas.hu>
2021-03-16 17:15:36 +0100

    Záróvizsga - Számjegyek done

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,5 +1,20 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
 
+    public int getCountOfDigits(String s) {
+        if (s == null) {
+            return 0;
+        }
+        Set<Character> digits = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            if (c > '0' && c < '9') {
+                digits.add(c);
+            }
+        }
+        return digits.size();
+    }
 }
